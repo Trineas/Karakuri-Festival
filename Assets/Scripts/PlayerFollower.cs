fc65c6b308f3931a058813f933121e7cead19c0c
Trineas <danny.sternbeck@gmail.com>
2019-11-22 20:31:23 +0100

    Cinemachine and PlayerFollower created

---
+++ Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+  
+public class PlayerFollower : MonoBehaviour 
+{
+  
+private Transform target;
+  
+     void Start() 
+     {
+        target = GameObject.Find("Player").transform;
+     }
+  
+     void Update() 
+     {
+        if (target.position.y == 8)
+        {
+        transform.position = new Vector3(target.position.x, target.position.y, target.position.z);
+        }
+
+        else if (target.position.y == 4)
+        {
+        transform.position = new Vector3(target.position.x, target.position.y, target.position.z);
+        }
+
+        else if ((target.position.y <= 0) && (target.position.y >= -3))
+        {
+        transform.position = new Vector3(target.position.x, target.position.y, target.position.z);
+        }
+
+        else
+        {
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+        }
+     }
+ }
